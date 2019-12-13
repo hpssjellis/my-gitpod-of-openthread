b455f6b93e16a169c9b87be9f6c025078e6689e2
Jeremy Ellis <keyfreemusic@gmail.com>
2019-12-13 09:40:52 -0800

    Update main.cpp

--- pre-compiled/platformIO/blink-with-serial/main.cpp
+++ pre-compiled/platformIO/blink-with-serial/main.cpp
@@ -1,6 +1,16 @@
+/*
+  blink-with-serial
+
+  Blinks the on board LED and tests serial output
+  
+  By Jeremy Ellis
+  http://www.rocksetta.com
+  Twitter @rocksetta
+*/
+
 #include <Arduino.h>
 
-
+// Done once
 void setup() {
   
   pinMode(LED_BUILTIN, OUTPUT);
@@ -9,21 +19,25 @@ void setup() {
 }
 
 
+// Loops forever
 void loop() {
-
-  int sensorValue = analogRead(A0);
+  
+  int sensorValue = analogRead(A0);  // A0 doesn't need to be connected
   float voltage = sensorValue * (5.0 / 1023.0);
   
   Serial.print("Serial proof showing A0 as a voltage: --> ");
   Serial.println(voltage);
   
+  // Now blink the built in LED
   digitalWrite(LED_BUILTIN, HIGH);   
-  delay(100);                      
+  delay(300);                      
   digitalWrite(LED_BUILTIN, LOW);    
-  delay(100);                    
+  delay(300);                    
 
   digitalWrite(LED_BUILTIN, HIGH);   
-  delay(100);                      
-  digitalWrite(LED_BUILTIN, LOW);    
-  delay(3000);                     
+  delay(300);                      
+  digitalWrite(LED_BUILTIN, LOW);   
+  delay(300);                      
+  
+  delay(3000);    // wait 3 seconds                 
 }
