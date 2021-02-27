d34566e6dc8042354ad87abe968d50c976dac0b6
Jeremy Ellis <keyfreemusic@gmail.com>
2021-02-26 23:52:15 -0800

    Update a04-vision120x120-grayscale.sh

--- testmotion/a04-vision120x120-grayscale.sh
+++ testmotion/a04-vision120x120-grayscale.sh
@@ -2,9 +2,9 @@
 
 unzip example-standalone-inferencing-ecm3532-master.zip
 
-mv ei-paint-brush-01-v3.zip example-standalone-inferencing-ecm3532-master/
+mv ei-paint-brush-01-v4.zip example-standalone-inferencing-ecm3532-master/
 cd example-standalone-inferencing-ecm3532-master
-unzip ei-paint-brush-01-v3.zip
+unzip ei-paint-brush-01-v4.zip
 make -j
 cp build/edge_impulse_inferencing_bootloader.bin ../eta/
 
