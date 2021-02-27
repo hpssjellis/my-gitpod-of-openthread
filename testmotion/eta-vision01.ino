cae9d159d8497a50b8b26e15cac1d9b7205828b8
Jeremy Ellis <keyfreemusic@gmail.com>
2021-02-26 23:11:16 -0800

    Create eta-vision01.ino

---
+++ testmotion/eta-vision01.ino
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+unzip example-standalone-inferencing-ecm3532-master.zip
+
+mv ei-paint-brush-01-v2.zip example-standalone-inferencing-ecm3532-master/
+cd example-standalone-inferencing-ecm3532-master
+unzip ei-paint-brush-01-v2.zip
+make -j
+cp build/edge_impulse_inferencing_bootloader.bin ../eta/
+
+echo "Download the ETA file to your computer that has edge-impulse-cli installed and activate the appropriate program"
