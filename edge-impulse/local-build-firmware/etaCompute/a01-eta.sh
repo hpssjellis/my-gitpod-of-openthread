e2c57fd56ab451be3d34a18c112064a136684ecb
Jeremy Ellis <keyfreemusic@gmail.com>
2021-02-28 16:49:27 -0800

    Update a01-eta.sh

--- edge-impulse/local-build-firmware/etaCompute/a01-eta.sh
+++ edge-impulse/local-build-firmware/etaCompute/a01-eta.sh
@@ -1,12 +1,21 @@
+#!/bin/bash
+
 echo "Upload your edge impulse zipped library to this directory and rename it as edge.zip"
+echo "enter to contrinue, ctrl-C to quit"
 read
+
+
+
 echo "unzipping and putting all the files into this file structure"
-unzip edge.zip myTempFolder/* 
-mv myTempFolder/* .
-rm -r myTempFolder
+unzip edge.zip 
+echo "In the src folder scheck the mani.cpp file and make changes or replace it with a different one"
+echo "enter to contrinue, ctrl-C to quit"
+read
+
+
 
 echo "Compiling your complete impulse"
-echo "This is most likely place to have issues"
+echo "This is the most likely place to have issues"
 make -j
 
 
@@ -15,6 +24,7 @@ cp build/edge_impulse_inferencing_bootloader.bin     ../eta-edge-serial-upload/
 
 
 echo "Download the full folder eta-edge-serial-upload so that you can install the new software on your ETA Compute device using the appropriate installer"
-echo "Edge impulse needs to be installed on your local machine"
+echo "Edge impulse client needs to be installed on your local machine"
+echo "npm install -g edge-impulse-cli"
 
 
