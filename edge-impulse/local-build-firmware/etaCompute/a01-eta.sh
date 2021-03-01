51ef1c2da7f2a4443fade95d905ff943302c580c
Jeremy Ellis <keyfreemusic@gmail.com>
2021-02-28 16:20:35 -0800

    Update a01-eta.sh

--- edge-impulse/local-build-firmware/etaCompute/a01-eta.sh
+++ edge-impulse/local-build-firmware/etaCompute/a01-eta.sh
@@ -1,1 +1,20 @@
-asdf
+echo "Upload your edge impulse zipped library to this directory and rename it as edge.zip"
+read
+echo "unzipping and putting all the files into this file structure"
+unzip edge.zip myTempFolder/* 
+mv myTempFolder/* .
+rm -r myTempFolder
+
+echo "Compiling your complete impulse"
+echo "This is most likely place to have issues"
+make -j
+
+
+echo "Copying the build/edge_impulse_inferencing_bootloader.bin to the downloadable folder eta-edge-serial-upload "
+cp build/edge_impulse_inferencing_bootloader.bin     ../eta-edge-serial-upload/
+
+
+echo "Download the full folder eta-edge-serial-upload so that you can install the new software on your ETA Compute device using the appropriate installer"
+echo "Edge impulse needs to be installed on your local machine"
+
+
