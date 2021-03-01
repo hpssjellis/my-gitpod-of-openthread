2b2bc68d25235d86dff608968bbc870dc22d4704
Jeremy Ellis <keyfreemusic@gmail.com>
2021-02-28 21:30:17 -0800

    Update a00-get-lora-example.sh

--- edge-impulse/local-build-firmware/portenta/a00-get-lora-example.sh
+++ edge-impulse/local-build-firmware/portenta/a00-get-lora-example.sh
@@ -6,6 +6,8 @@ git clone https://github.com/edgeimpulse/example-portenta-lorawan.git
 
 cd example-portenta-lorawan
 
+echo "make changes to the src/main.cpp files, save and hit enter or ctrl-c"
+
 ./arduino-build.sh --build
 
 
